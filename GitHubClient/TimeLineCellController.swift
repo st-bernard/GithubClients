1a2f9d8615a46ca798ffcd011eae3814788537d6
ShionKoga <shion_koga@mail.toyota.co.jp>
2021-12-15 17:57:15 +0900

    replace UITableViewController

--- GitHubClient/TimeLineCellController.swift
+++ GitHubClient/TimeLineCellController.swift
@@ -1,19 +1,14 @@
 import Foundation
 import UIKit
 
-final class TimeLineViewController: UIViewController {
+final class TimeLineViewController: UITableViewController {
     
     private var viewModel: UserListViewModel!
-    private var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView = UITableView(frame: view.bounds, style: .plain)
-        tableView.delegate = self
-        tableView.dataSource = self
+
         tableView.register(TimeLineCell.self, forCellReuseIdentifier: TimeLineCell.id)
-        self.view.addSubview(tableView)
         
         viewModel = UserListViewModel()
         viewModel.stateDidUpdate = { [weak self] state in
@@ -37,24 +32,20 @@ final class TimeLineViewController: UIViewController {
         }
         viewModel.getUsers()
     }
-    
-    @objc private func refreshControlValueDidChange(sender: UIRefreshControl) {
-        viewModel.getUsers()
-    }
 }
 
-extension TimeLineViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+extension TimeLineViewController {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 75
     }
 }
 
-extension TimeLineViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension TimeLineViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.viewModel.usersCount()
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let timeLineCell = tableView.dequeueReusableCell(withIdentifier: TimeLineCell.id) as? TimeLineCell {
             let cellViewModel = self.viewModel.cellViewModels[indexPath.row]
             timeLineCell.setNickName(nickName: cellViewModel.nickName)
